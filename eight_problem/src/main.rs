2fd8e74e2d26314683ede1acc838068d73acc141
BuildTools <mariothrowsfieball@gmail.com>
2023-12-08 12:14:20 +0100

    solved day 8

--- eight_problem/src/main.rs
+++ eight_problem/src/main.rs
@@ -1,25 +1,16 @@
 use rayon::prelude::*;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::str::Chars;
 mod input;
 
 struct Node<'a, 'b> {
     left: &'a str,
     right: &'b str,
-    // distance_from_z: Option<i32>,
 }
 
 impl<'a, 'b> Node<'a, 'b> {
     fn new(left: &'a str, right: &'b str) -> Node<'a, 'b> {
-        // let mut distance_from_z = None;
-        // if left == "ZZZ" || right == "ZZZ" {
-        //     distance_from_z = Some(1)
-        // }
-        Node {
-            left,
-            right,
-            // distance_from_z,
-        }
+        Node { left, right }
     }
 }
 
@@ -45,7 +36,7 @@ fn parse_nodes(input: &str) -> Vec<[String; 3]> {
         .collect()
 }
 
-fn create_hash_map<'a>(nodes: &'a Vec<[String; 3]>) -> HashMap<&'a String, Node> {
+fn create_hash_map(nodes: &[[String; 3]]) -> HashMap<&String, Node> {
     let mut hash_map = HashMap::new();
     nodes.iter().for_each(|[node, left, right]| {
         hash_map.insert(node, Node::new(left, right));
@@ -53,7 +44,7 @@ fn create_hash_map<'a>(nodes: &'a Vec<[String; 3]>) -> HashMap<&'a String, Node>
     hash_map
 }
 
-fn solve_part_1(input: &str) -> i32 {
+fn solve_part_1(input: &str) -> u64 {
     let mut splitted = input.split("\n\n");
     let instructions: Vec<char> = splitted
         .next()
@@ -73,7 +64,7 @@ fn solve_part_1(input: &str) -> i32 {
     i
 }
 
-fn solve_part_2(input: &str) -> i32 {
+fn solve_part_2(input: &str) -> u64 {
     let mut splitted = input.split("\n\n");
     let instructions: Vec<char> = splitted
         .next()
@@ -81,26 +72,40 @@ fn solve_part_2(input: &str) -> i32 {
         .chars()
         .collect();
     let nodes = parse_nodes(splitted.next().unwrap());
-    let map = create_hash_map(&nodes);
-    let mut i = 0;
-    let mut current_nodes: Vec<String> = nodes
-        .iter()
-        .filter_map(|node| {
-            if node[0].ends_with('A') {
-                Some(node[0].clone())
-            } else {
-                None
-            }
-        })
+    let nodes_clone = nodes.clone();
+    let start_nodes = nodes_clone
+        .into_iter()
+        .filter(|x| x[0].ends_with('A'))
         .collect();
+    let map = create_hash_map(&nodes);
+    let cycle_lengths = get_cycles(instructions, &start_nodes, &map);
+    println!("{:?}", cycle_lengths);
+    least_common_multiple(cycle_lengths)
+}
+
+fn get_cycles(
+    instructions: Vec<char>,
+    nodes: &Vec<[String; 3]>,
+    map: &HashMap<&String, Node>,
+) -> Vec<u64> {
+    let mut result = Vec::new();
+    for node in nodes {
+        result.push(get_cycle_length(&instructions, node[0].as_str(), map));
+    }
+    result
+}
+
+fn get_cycle_length<'a>(
+    instructions: &'a Vec<char>,
+    mut node: &'a str,
+    map: &'a HashMap<&String, Node>,
+) -> u64 {
+    let mut i = 0;
     loop {
         for instruction in instructions.iter() {
-            current_nodes = current_nodes
-                .par_iter()
-                .map(|node| get_next_node(instruction, &map, node).to_string())
-                .collect();
+            node = get_next_node(instruction, map, node);
             i += 1;
-            if do_all_end_with_z(&current_nodes) {
+            if node.ends_with('Z') {
                 return i;
             };
         }
@@ -108,11 +113,13 @@ fn solve_part_2(input: &str) -> i32 {
 }
 
 fn do_all_end_with_z(nodes: &Vec<String>) -> bool {
-    let node_that_end_with_z: Vec<&String> = nodes
-        .par_iter()
-        .filter(|node| node.ends_with('Z'))
-        .collect();
-    node_that_end_with_z.len() == nodes.len()
+    let result = nodes.par_iter().try_for_each(|node| {
+        if !node.ends_with('Z') {
+            return None;
+        };
+        Some(())
+    });
+    result.is_some()
 }
 
 fn get_next_node<'a>(
@@ -133,6 +140,48 @@ fn get_next_node<'a>(
         }
         _ => panic!("invalid instruction"),
     }
+}
+
+fn least_common_multiple(mut numbers: Vec<u64>) -> u64 {
+    let mut prime_factors = HashSet::new();
+    for number in numbers {
+        for prime in find_prime_factors(number) {
+            prime_factors.insert(prime);
+        }
+    }
+    prime_factors.iter().product()
+}
+
+fn find_prime_factors(mut number: u64) -> Vec<u64> {
+    let mut prime_factors = vec![];
+
+    for divisor in 2..=((number as f64).sqrt()) as u64 {
+        if is_prime(divisor) {
+            while number % divisor == 0 {
+                if !prime_factors.contains(&divisor) {
+                    prime_factors.push(divisor);
+                }
+                number /= divisor;
+            }
+        }
+    }
+    prime_factors.push(number);
+    prime_factors
+}
+
+fn is_prime(number: u64) -> bool {
+    if number == 1 || number == 2 {
+        return true;
+    }
+    if number % 2 == 0 {
+        return false;
+    }
+    for i in 2..=((number as f64).sqrt()) as u64 {
+        if number % i == 0 {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
