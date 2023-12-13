c3723f872411bb894bb997e470462c02dc7319b6
BuildTools <mariothrowsfieball@gmail.com>
2023-12-13 13:50:05 +0100

    solved part 1 brut force

--- twelve_day/src/main.rs
+++ twelve_day/src/main.rs
@@ -1,26 +1,66 @@
 use rayon::prelude::*;
 use std::collections::HashSet;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 mod input;
 
 fn main() {
+    let result = part_2_input_generate(input::INPUT);
+    println!("{:?}", result);
+    let now = Instant::now();
     let result = how_many_arragments(input::INPUT);
     println!("{:?}", result);
+    println!("{} seconds", now.elapsed().as_secs());
+}
+
+fn solve_part_2(input: &str) -> usize {
+    let input = part_2_input_generate(input);
+    how_many_arragments(input.as_str())
+}
+
+fn part_2_input_generate(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let splitted: Vec<&str> = line.split(' ').collect();
+            let first_part = (0..5).fold(String::new(), |acc, i| {
+                if i != 4 {
+                    acc + splitted[0] + "?"
+                } else {
+                    acc + splitted[0]
+                }
+            });
+            let second_part = (0..5).fold(String::new(), |acc, i| {
+                if i != 4 {
+                    acc + splitted[1] + ","
+                } else {
+                    acc + splitted[1]
+                }
+            });
+            let mut result = first_part + " " + second_part.as_str();
+            result.push('\n');
+            result
+        })
+        .collect()
 }
 
 fn how_many_arragments(input: &str) -> usize {
     let rows = parse_input(input);
-    // let length = rows.len();
-    // println!("{:?}", length);
-    // let result = rows.iter().enumerate().fold(0, |acc, (i, row)| {
-    //     println!("{:?}", i);
-    //     let values: Vec<usize> = row[1]
-    //         .split(',')
-    //         .map(|x| x.parse::<usize>().unwrap())
-    //         .collect();
-    //     let mut set = HashSet::new();
-    //     acc + insert_all(row[0], &values.clone(), values, &mut set)
-    // });
+    // rows.iter()
+    //     .enumerate()
+    //     .map(|(i, row)| {
+    //         let values: Vec<usize> = row[1]
+    //             .split(',')
+    //             .map(|x| x.parse::<usize>().unwrap())
+    //             .collect();
+    //         let mut set = HashSet::new();
+    //         let mut seen = HashSet::new();
+    //         let number_of_arragements =
+    //             insert_all(row[0], &values.clone(), values, &mut set, &mut seen);
+    //         number_of_arragements
+    //     })
+    //     .sum()
     let result = rows
         .par_iter()
         .fold(
@@ -32,7 +72,10 @@ fn how_many_arragments(input: &str) -> usize {
                     .collect();
                 let mut set = HashSet::new();
                 let mut seen = HashSet::new();
-                acc + insert_all(row[0], &values.clone(), values, &mut set, &mut seen)
+                let number_of_arragements =
+                    insert_all(row[0], &values.clone(), values, &mut set, &mut seen);
+
+                acc + number_of_arragements
             },
         )
         .reduce(|| 0, |current, acc| current + acc);
@@ -50,28 +93,15 @@ fn insert_all(
         return 0;
     }
     seen.insert(springs.to_string());
-    // println!("{:?}", springs);
     let unknows = all_unknwons(springs);
-    let mut result = springs.to_string();
-    let broken_in_string = springs.chars().filter(|x| x == &'#').collect::<Vec<char>>();
-    if broken_in_string.len() == original_records.iter().sum()
-        && how_many_broken_sequences(springs) == original_records.len()
-    {
-        if !unknows.is_empty() {
-            result = springs.replace('?', ".");
-        }
+    let result = springs.replace('?', ".");
+    if &how_many_broken_sequences(result.as_str()) == original_records {
         set.insert(result);
-        return 0;
     }
-    let mut seen_record = HashSet::new();
     for record in records.iter() {
-        if seen_record.get(&record).is_some() {
-            continue;
-        }
-        seen_record.insert(record);
         for unknown in unknows.iter() {
             if let Some(updated_spring) = insert_broken(springs, *unknown, *record) {
-                let records = (&records[1..records.len()]).to_vec();
+                let records = (records[1..records.len()]).to_vec();
                 insert_all(
                     updated_spring.as_str(),
                     original_records,
@@ -82,6 +112,7 @@ fn insert_all(
             };
         }
     }
+
     if set.is_empty() {
         1
     } else {
@@ -99,44 +130,47 @@ fn all_unknwons(springs: &str) -> Vec<usize> {
     result
 }
 
-// for this input ##..## it returns 2
-fn how_many_broken_sequences(springs: &str) -> usize {
-    let mut result = 0;
+fn how_many_broken_sequences(springs: &str) -> Vec<usize> {
+    let mut result = vec![];
     let spltted_spring: Vec<char> = springs.chars().collect();
+    let mut current_sequence_length = 0;
     for (i, _) in spltted_spring.iter().enumerate() {
-        if i == 0 {
-            if spltted_spring[i] == '#' {
-                result += 1;
+        if spltted_spring[i] == '#' {
+            current_sequence_length += 1;
+            if i + 1 == springs.len() || spltted_spring[i + 1] != '#' {
+                result.push(current_sequence_length);
+                current_sequence_length = 0;
             }
-        } else if spltted_spring[i - 1] == '.' && spltted_spring[i] == '#' {
-            result += 1;
         }
     }
     result
 }
 
-fn insert_broken(springs: &str, index: usize, amount: usize) -> Option<String> {
+fn insert_broken(springs: &str, mut index: usize, mut amount: usize) -> Option<String> {
     let mut springs_char = springs.chars().collect::<Vec<char>>();
-    if index != 0 && springs_char[index - 1] == '#' {
-        return None;
+    let mut starting: i32 = 0;
+    while index > starting as usize && springs_char[index - starting as usize - 1] == '#' {
+        starting += 1;
     }
+    let adjusted_index = (index as i32 - starting) as usize;
+
     for i in 0..amount {
-        if index + i == springs_char.len() {
+        if adjusted_index + i == springs_char.len() {
             return None;
         }
-        if springs_char[index + i] == '.' {
+        if springs_char[adjusted_index + i] == '.' {
             return None;
         }
-        if index != 0 && springs_char[index - 1] == '?' {
-            springs_char[index - 1] = '.';
+        if adjusted_index != 0 && springs_char[adjusted_index - 1] == '?' {
+            springs_char[adjusted_index - 1] = '.';
         }
-        springs_char[index + i] = '#';
+        springs_char[adjusted_index + i] = '#';
     }
-    if index + amount != springs_char.len() {
-        if springs_char[index + amount] == '#' {
+    if adjusted_index + amount < springs_char.len() {
+        if springs_char[adjusted_index + amount] == '#' {
             return None;
         }
-        springs_char[index + amount] = '.';
+        springs_char[adjusted_index + amount] = '.';
     }
     Some(springs_char.iter().collect::<String>())
 }
@@ -155,31 +189,31 @@ fn parse_input(input: &str) -> Vec<[&str; 2]> {
 mod tests {
     use super::*;
     #[test]
-    fn first_exapmel_part_1() {
+    fn test_first_line_part_1() {
         let input = "???.### 1,1,3";
         let result = how_many_arragments(input);
         assert_eq!(1, result);
     }
     #[test]
-    fn second_example_part_1() {
+    fn test_second_line_part_1() {
         let input = ".??..??...###. 1,1,3";
         let result = how_many_arragments(input);
         assert_eq!(4, result);
     }
     #[test]
-    fn third_example_part_1() {
+    fn test_third_line_part_1() {
         let input = "?###???????? 3,2,1";
         let result = how_many_arragments(input);
         assert_eq!(10, result);
     }
     #[test]
-    fn third_example_part_4() {
+    fn test_fort_line_part_1() {
         let input = "?#?#?#?#?#?#?#? 1,3,1,6";
         let result = how_many_arragments(input);
         assert_eq!(1, result);
     }
     #[test]
-    fn third_example_part_5() {
+    fn test_fifth_line_part_1() {
         let input = "????.#...#... 4,1,1";
         let result = how_many_arragments(input);
         assert_eq!(1, result);
@@ -195,5 +229,71 @@ mod tests {
 ";
         let result = how_many_arragments(input);
         assert_eq!(21, result);
+    }
+    #[test]
+    fn part_2_test() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+";
+        let result = solve_part_2(input);
+        assert_eq!(525152, result);
+    }
+    #[test]
+    fn input_test_1() {
+        let input = "???#...#.?#??#?#? 1,1,1,8";
+        let result = how_many_arragments(input);
+        assert_eq!(2, result);
+    }
+    #[test]
+    fn input_test_2() {
+        let input = "#?.???????#????#???. 1,1,12";
+        let result = how_many_arragments(input);
+        assert_eq!(6, result);
+    }
+    #[test]
+    fn input_test_3() {
+        let input = "????.#??##??? 1,2,3";
+        let result = how_many_arragments(input);
+        assert_eq!(4, result);
+    }
+    #[test]
+    fn input_test_4() {
+        let input = "??????.?.???##?#?. 6,1,7";
+        let result = how_many_arragments(input);
+        assert_eq!(2, result);
+    }
+    #[test]
+    fn input_test_5() {
+        let input = "#??????????##?.??? 5,1,1,3,1,1";
+        let result = how_many_arragments(input);
+        assert_eq!(4, result);
+    }
+    #[test]
+    fn input_test_6() {
+        let input = "??.#?.?.?#?.? 1,1";
+        let result = how_many_arragments(input);
+        assert_eq!(1, result);
+    }
+    #[test]
+    fn broken_sequence_test_1() {
+        let input = "#..#.############...";
+        let result = how_many_broken_sequences(input);
+        assert_eq!(vec![1, 1, 12], result);
+    }
+    #[test]
+    fn insert_test_1() {
+        let input = "#.??.#??##???";
+        let result = insert_broken(input, 6, 2);
+        assert_eq!(Some("#.??.##.##???".to_string()), result);
+    }
+    #[test]
+    fn parse_input_2() {
+        let input = ".# 1";
+        let result = part_2_input_generate(input);
+        assert_eq!(result, ".#?.#?.#?.#?.# 1,1,1,1,1\n");
     }
 }
